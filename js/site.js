e87af63537f7aae423eebb917d951de91f92f6e1
rebcop <76892971+rebcop@users.noreply.github.com>
2023-10-17 00:26:09 -0400

    Completed challenge

--- js/site.js
+++ js/site.js
@@ -1,12 +1,87 @@
-function displayMessage() {
-    // get the text out of our <input> and assign it to a variable
-    let msg = document.getElementById('message').value;
+// get message from user input to check if it's palindrome
+function getValues() {
 
-    Swal.fire(
-        {
+    // get message input from the page
+    let message = document.getElementById('message').value;
+
+    // validate the input: make sure the input is not empty
+    if ( message.length == 0 ) {
+        swal.fire({
+            icon: 'error',
             backdrop: false,
-            title: 'APP NAME',
-            text: msg, // <-- replace this text with that variable
+            text: 'Please enter a message to check.'
+        })
+    } else {
+
+        // pass the message to checkForPalindrome and assign its return
+        let palindromeMsg = checkForPalindrome(message);
+
+        // give the palindrome message to display results to show on the page
+        displayResults(palindromeMsg);
+    }
+
+}
+
+// check the user input grabbed by getValues to see if it is a palindrome
+function checkForPalindrome(message) {
+
+    let msgClean = '';
+
+    let msgCleanRev ='';
+
+    let regex = /[A-Za-z0-9]/;
+
+    let output = [];
+
+    for (let i = 0; i < message.length; i++) {
+
+        if (regex.test(message[i])) {
+
+            msgClean += message[i].toLowerCase();
+
         }
-    );
+
+    }
+
+    for (let i = msgClean.length - 1; i >= 0; i--) {
+
+        msgCleanRev += msgClean[i];
+
+    }
+    
+    if ( msgClean != '' && msgClean == msgCleanRev ) {
+
+        output.push(true);
+        output.push(msgCleanRev);
+
+
+    } else {
+        output.push(false);
+        output.push(msgClean);
+        output.push(msgClean.length*5 % 100 + 20);
+    }
+
+    return output;
+
+}
+
+// display result of checkForPalindrome back to user
+function displayResults(message) {
+
+    if (message[0]) {
+        document.getElementById('results').textContent = 'You entered a Palindrome! Take a breather.';
+        document.getElementById('msg').textContent = `Your message reversed is: ${message[1]}`;
+        document.getElementById('alert').classList.add('alert-success');
+        document.getElementById('alert').classList.remove('alert-danger');
+
+    } else {
+        document.getElementById('results').textContent = `You did not enter a Palindrome! Drop and Give Me ${message[2]}!`;
+        document.getElementById('msg').textContent = `Your message reversed is: ${message[1]}`;
+        document.getElementById('alert').classList.remove('alert-success');
+        document.getElementById('alert').classList.add('alert-danger');
+
+    }
+
+    document.getElementById('alert').classList.remove('invisible');
+
 }
