d4393db4460ec1facc042889a4b538999818935f
rebcop <76892971+rebcop@users.noreply.github.com>
2023-10-20 14:04:05 -0400

    Completed the code page.

--- js/site.js
+++ js/site.js
@@ -34,35 +34,37 @@ function checkForPalindrome(message) {
 
     let output = [];
 
+    // let regex = /[^A-Za-z0-9]/;
+    // msgClean = message.replace(regex,'').toLowerCase();
+
     // Take out all the characters from the input from the user that are not a letter or number
+    // Could have used replace method instead commented out above
     for (let i = 0; i < message.length; i++) {
 
         if (regex.test(message[i])) {
 
             msgClean += message[i].toLowerCase();
-
         }
-
     }
 
     // Reverse the clean message
     for (let i = msgClean.length - 1; i >= 0; i--) {
-
         msgCleanRev += msgClean[i];
-
     }
     
-    // 
+    // If there is nothing in the clean message return error
     if ( msgClean == '' ) {
         output.push('error');
         return output;
-
+    
+    // If the reverse of the clean message is equal to the clean message it's a palindrome!
     } else if ( msgClean == msgCleanRev ) {
         output.push(true);
         output.push(msgCleanRev);
         return output;
     }
     
+    // If it's anything else, it's not a palindrome
     else {
         output.push(false);
         output.push(msgCleanRev);
@@ -70,39 +72,39 @@ function checkForPalindrome(message) {
         return output;
     }
 
+    // Use objects to return multiple values instead of arrays so you can access them by name
+    // let reversed = reverseMessage(cleanInput);
+    // let isPalindrome = reversed == cleanInput;
+
+    // let output = {
+    //     revMsg: '', // Reversed clean input from user
+    //     isPalindrome: isPalindrome // true or false or error
+    // }
 }
 
 // display result of checkForPalindrome back to user
 function displayResults(message) {
+
+    // Remove classes
+    document.getElementById('alert').classList.remove('invisible', 'alert-danger', 'alert-warning', 'alert-success');
 
     // Display success message if the input from user is a palindrome
     if (message[0] == true) {
         document.getElementById('results').textContent = 'A Palindrome! Take a breather.';
         document.getElementById('msg').textContent = `Your message reversed is: ${message[1]}`;
         document.getElementById('alert').classList.add('alert-success');
-        document.getElementById('alert').classList.remove('alert-danger');
-        document.getElementById('alert').classList.remove('alert-warning');
 
     // Display error message if the input from user does not have any letters or numbers to check
     } else if ( message[0] == 'error') {
         document.getElementById('results').textContent = 'Error!';
         document.getElementById('msg').textContent = `Please enter letters or numbers`;
         document.getElementById('alert').classList.add('alert-warning');
-        document.getElementById('alert').classList.remove('alert-success');
-        document.getElementById('alert').classList.remove('alert-danger');
     }
     
     // Display fail message if the input from user is not a palindrome
     else {
         document.getElementById('results').textContent = `Not a Palindrome! Drop and Give Me ${message[2]}!`;
         document.getElementById('msg').textContent = `Your message reversed is: ${message[1]}`;
-        document.getElementById('alert').classList.remove('alert-success');
-        document.getElementById('alert').classList.remove('alert-warning');
         document.getElementById('alert').classList.add('alert-danger');
-
     }
-
-    // Make the results show on the page
-    document.getElementById('alert').classList.remove('invisible');
-
 }
